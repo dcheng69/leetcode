8406db2776659e15d75c3bf68ef1611a80f8e712
clay <dcheng69@uwo.ca>
2023-11-14 14:27:13 -0500

    leetcode 102 queue implementation
    [Ticket: feature 102]
    
    [body]
    1. using queue to help level traverse
    
    [test]
    1. pass all test cases, leecode runtime Beats 14.55%, need futher
       optimization

---
+++ 102_Binary_Tree_Level_Order_Traversal/Solution102.h
@@ -0,0 +1,13 @@
+#ifndef SOLUTION102_H_
+#define SOLUTION102_H_
+#include <vector>
+#include "TreeNode.h"
+using std::vector;
+
+class Solution {
+    public:
+        vector<vector<int>> levelOrder(TreeNode* root);
+
+};
+
+#endif
