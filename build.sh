1ce9cf0be5759ea638b7ded5b9d606cd5001170e
clay <dcheng69@uwo.ca>
2023-12-17 08:07:23 -0500

    Modify the build script and gitignore file
    [Ticket: script]
    
    [body]
    1. modify the build script to support windows and ubuntu compile
    2. add a new gitignore file
    
    [test]
    1. compile pass

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --build build
-cp ./build/compile_commands.json ./
-# cd build && ctest
+
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <folder>"
+  exit 1
+fi
+
+folder=$1
+
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B "$folder"
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --build "$folder"
+cp "$folder/compile_commands.json" .
+
+# Uncomment the following line if you want to run tests using ctest
+# cd "$folder/build" && ctest
