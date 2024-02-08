40f2f5fdfd9c29d7be7f22ea2f5cefdbe6ee0cd0
clay <dcheng69@uwo.ca>
2024-02-08 13:55:54 -0500

    leetcode 450 partly recursive solution
    [Ticket: feature 450]
    
    [body]
    1. Add a partly recursive solution for this problem
    
    [test]
    1. pass all local test cases, leetcode submit success

--- 450_Delete_Node_in_a_BST/Solution450.cc
+++ 450_Delete_Node_in_a_BST/Solution450.cc
@@ -1,9 +1,13 @@
 #include "Solution450.h"
 #include "TreeNode.h"
+#include <iostream>
 #include <stack>
 #include <vector>
 using namespace std;
+//#define DEBUG
+#define SOLUTION_2
 
+#ifdef SOLUTION_1
 /**
  * @brief Recursive version of the code
  *
@@ -45,7 +49,84 @@ TreeNode* Solution::deleteNode(TreeNode* root, int key) {
     }
     return root;
 }
+#endif // SOLUTION_1
+       //
+#ifdef SOLUTION_2
+/**
+ * @brief Non recursive, or partly recursive version of the code
+ *
+ * @param root
+ * @param key
+ *
+ * @return
+ */
+TreeNode* Solution::deleteNode(TreeNode* root, int key) {
+    if (!root)
+        return nullptr;
 
+    TreeNode* last = nullptr;
+    TreeNode* curr = root;
+    bool find = false;
+    while (curr) {
+        if (curr->val > key) {
+            // search in the left child TreeNode
+#ifdef DEBUG
+            cout << "turn to the left node! \n";
+#endif // !DEBUG
+            last = curr;
+            curr = curr->left;
+        } else if (curr->val < key) {
+            // search in the right child TreeNode
+#ifdef DEBUG
+            cout << "turn to the right node! \n";
+#endif // !DEBUG
+            last = curr;
+            curr = curr->right;
+        } else if (curr->val == key) {
+            // find the node with the key value
+#ifdef DEBUG
+            cout << "Node found is:" << curr->val << endl;
+#endif // !DEBUG
+            find = true;
+            break;
+        }
+    }
+
+    if (find) {
+        // leaf node
+        if (!curr->left && !curr->right) {
+            if (!last) {
+                return nullptr;
+            } else if (last->left == curr) {
+                last->left = nullptr;
+            } else if (last->right == curr) {
+                last->right = nullptr;
+            }
+        } else if (curr->left && curr->right) {
+            // two child nodes
+            TreeNode* temp = curr->left;
+            while (temp->right) {
+                temp = temp->right;
+            }
+
+            curr->val = temp->val;
+            curr->left = deleteNode(curr->left, temp->val);
+        } else {
+            // only one child node
+            if (!last) {
+                return curr->left ? curr->left : curr->right;
+            } else if (last->left == curr) {
+                last->left = curr->left ? curr->left : curr->right;
+            } else if (last->right == curr) {
+                last->right = curr->left ? curr->left : curr->right;
+            }
+        }
+    }
+
+    return root;
+}
+
+#endif // SOLUTION_2
 
 /**
  * @brief Non recursive version of inorderTraverasl
